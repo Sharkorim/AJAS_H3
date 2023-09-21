cb95a2f855ff51a349df9763627e72432b216cd0
sharkorim <saidaugustoo.so@gmail.com>
2023-09-21 11:53:23 -0400

    HACKS h3/h7

--- hack_3/hack_3.js
+++ hack_3/hack_3.js
@@ -8,6 +8,18 @@
 */
 let arr = [{a:1},{b:2},{c:3},{d:4},{e:5}];
 let result = [];
+let i = 0;
+
+while (i < arr.length) {
+    if (i % 2 === 0) {
+        let obj = arr[i];
+        for (let key in obj) {
+            result.push(obj[key]);
+        }
+    }
+    i++;
+}
+
  
  
 //export result
--- hack_7/hack_7.js
+++ hack_7/hack_7.js
@@ -10,7 +10,8 @@ let foo = {
     role:"admin",
     skills:["javascript", "html", "css", "python", "flask", "react", "redux"]
 };
-let result;
+let result = foo.skills.map(skill => skill);
+console.log(result);
 
 
 //export result
