b07ae89a52a1654f7ae47ab06fab29f0fe93b7e5
AnaMCarrenoA <128761853+AnaMCarrenoA@users.noreply.github.com>
2023-09-22 22:44:21 -0400

    Update Ana_hack_4.js

--- hack_4/hack_4.js
+++ hack_4/hack_4.js
@@ -11,6 +11,12 @@ let tokenF = {f:6};
 let tokenG = {g:7};
 let result = [];
  
- 
+result.push(tokenG);
+arr.forEach((element) => {
+    result.push(element);
+});
+result.push(tokenF);
+
+console.log(result); // [{g:7},{a:1},{b:2},{c:3},{d:4},{e:5},{f:6}]
 //export result
 module.exports = result; 
